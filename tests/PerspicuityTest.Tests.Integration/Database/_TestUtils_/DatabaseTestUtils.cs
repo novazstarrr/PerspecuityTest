2c2c9347d5f5d1f2401255967b447b41ce847e4e
kennmillard@gmail.com <kennmillard@gmail.com>
2023-05-17 14:01:15 +0100

    Updated Integrations Testing

---
+++ tests/PerspicuityTest.Tests.Integration/Database/_TestUtils_/DatabaseTestUtils.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace PerspicuityTest.Tests.Integration.Database._TestUtils_
+{
+    public static class DatabaseTestUtils
+    {
+        private static string ConnectionString { get; }
+
+        static DatabaseTestUtils()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("Web.Api/appsettings.test.json")
+                .Build();
+
+            ConnectionString = configuration.GetConnectionString("Database")!;
+        }
+
+        public static async Task ResetDatabase()
+        {
+            using var sqlConnection = new SqlConnection(ConnectionString);
+            await sqlConnection.OpenAsync();
+
+            var deleteQuery = @"
+                DELETE dbo.Classes;
+            ";
+
+            using var command = new SqlCommand(deleteQuery, sqlConnection);
+            await command.ExecuteNonQueryAsync();
+        }
+    }
+}
--- tests/PerspicuityTest.Tests.Integration/Web.Api/Controllers/ClassesControllerTests.cs
+++ tests/PerspicuityTest.Tests.Integration/Web.Api/Controllers/ClassesControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
 using Newtonsoft.Json;
 using PerspicuityTest.Core.Dtos;
+using PerspicuityTest.Tests.Integration.Database._TestUtils_;
 using PerspicuityTest.Tests.Integration.Web.Api._TestUtils_;
 using System.Net;
 using System.Net.Http.Json;
@@ -14,6 +15,7 @@ namespace PerspicuityTest.Tests.Integration.Web.Api.Controllers
 
         public ClassesControllerTests(TestPerspicuityTestWebApi factory)
         {
+            DatabaseTestUtils.ResetDatabase().Wait();
             _client = factory.CreateClient();
         }
 
@@ -34,6 +36,33 @@ namespace PerspicuityTest.Tests.Integration.Web.Api.Controllers
             Assert.Equal(data.Start, newClass.Start);
             Assert.Equal(data.End, newClass.End);
             Assert.NotNull(data.Id);
+        }
+
+        [Fact]
+        public async Task Get_returns_all_classes()
+        {
+            var class1 = new Class(null, "Class1", new DateTime(2023, 5, 17, 9, 0, 0), new DateTime(2023, 5, 17, 10, 0, 0));
+            var class2 = new Class(null, "Class2", new DateTime(2023, 5, 20, 11, 0, 0), new DateTime(2023, 5, 20, 12, 0, 0));
+
+            await _client.PostAsJsonAsync("/api/classes", class1);
+            await _client.PostAsJsonAsync("/api/classes", class2);
+
+            var classesResponse = await _client.GetFromJsonAsync<List<Class>>("/api/classes");
+
+            Assert.NotNull(classesResponse);
+            Assert.Equal(2, classesResponse.Count);
+
+            var class1Response = classesResponse.Single(c => c.Name == class1.Name);
+            Assert.Equal(class1Response.Name, class1.Name);
+            Assert.Equal(class1Response.Start, class1.Start);
+            Assert.Equal(class1Response.End, class1.End);
+            Assert.NotNull(class1Response.Id);
+
+            var class2Response = classesResponse.Single(c => c.Name == class2.Name);
+            Assert.Equal(class2Response.Name, class2.Name);
+            Assert.Equal(class2Response.Start, class2.Start);
+            Assert.Equal(class2Response.End, class2.End);
+            Assert.NotNull(class2Response.Id);
         }
     }
 }
